65a1ee405f99a04bf45605a390ca72510d15e15d
Michael O'Cleirigh <michael.ocleirigh@rivulet.ca>
2011-07-09 13:07:44 -0400

    implement github authorization strategy
    This is the top level access strategy.  We allow a comma delimited list of usernames to be given for the administrator users.
    
    We also have a check box that can be used to let authenticated users have a read only view.
    
    This would allow any github user to view the jobs and settings.

--- src/main/java/org/jenkinsci/plugins/GithubAuthorizationStrategy.java
+++ src/main/java/org/jenkinsci/plugins/GithubAuthorizationStrategy.java
@@ -3,22 +3,22 @@
  */
 package org.jenkinsci.plugins;
 
+import hudson.Extension;
+import hudson.model.Descriptor;
+import hudson.security.ACL;
+import hudson.security.AuthorizationStrategy;
+import hudson.security.Permission;
+
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 import net.sf.json.JSONObject;
 
 import org.acegisecurity.Authentication;
+import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
-
-import hudson.Extension;
-import hudson.model.Descriptor;
-import hudson.model.Descriptor.FormException;
-import hudson.security.ACL;
-import hudson.security.AuthorizationStrategy;
-import hudson.security.LegacyAuthorizationStrategy;
-import hudson.security.Messages;
-import hudson.security.Permission;
 
 /**
  * @author mocleiri
@@ -30,28 +30,63 @@ public class GithubAuthorizationStrategy extends AuthorizationStrategy {
 
 	private static class GithubACL extends ACL {
 
+		private final List<String> adminUserNameList;
+		private final boolean authenticatedUserReadPermission;
+
 		@Override
 		public boolean hasPermission(Authentication a, Permission permission) {
-			// TODO Auto-generated method stub
-			return true;
+			
+			String p = a.getName();
+			
+			if (adminUserNameList.contains(p)) {
+				// if they are an admin then they have permission
+				return true;
+			}
+			else {
+				if (authenticatedUserReadPermission) {
+					if (permission.equals(Permission.READ))
+						// if we support authenticated read and this is a read request we allow it
+						return true;
+				}
+			}
+			
+			// all other cases we don't allow access.
+			return false;
 		}
 
-		public GithubACL() {
+		public GithubACL(List<String> adminUserNameList, boolean authenticatedUserReadPermission) {
 			super();
-			// TODO Auto-generated constructor stub
+			
+			this.authenticatedUserReadPermission = authenticatedUserReadPermission;
+			this.adminUserNameList = adminUserNameList;
 		}
 		
 		
 		
 	}
 	
-	private static GithubACL ROOT_ACL = new GithubACL();
+	private List<String> adminUserNameList;
+
+	private final boolean authenticatedUserReadPermission;
 	
 	/**
 	 * 
 	 */
-	public GithubAuthorizationStrategy() {
-		// TODO Auto-generated constructor stub
+	public GithubAuthorizationStrategy(String adminUserNames, boolean authenticatedUserReadPermission) {
+		super();
+		this.authenticatedUserReadPermission = authenticatedUserReadPermission;
+		
+		this.adminUserNameList = new LinkedList<String>();
+		
+		String[] parts = adminUserNames.split(",");
+		
+		for (String part : parts) {
+			adminUserNameList.add(part);
+		}
+		
+		
+		
+		
 	}
 
 	/* (non-Javadoc)
@@ -59,7 +94,7 @@ public class GithubAuthorizationStrategy extends AuthorizationStrategy {
 	 */
 	@Override
 	public ACL getRootACL() {
-		return ROOT_ACL;
+		return new GithubACL(this.adminUserNameList, this.authenticatedUserReadPermission);
 	}
 
 	/* (non-Javadoc)
@@ -80,11 +115,18 @@ public class GithubAuthorizationStrategy extends AuthorizationStrategy {
             return "/help/security/github-committer-auth-strategy.html";
         }
 
+        
+        
 		@Override
 		public GithubAuthorizationStrategy newInstance(StaplerRequest req,
 				JSONObject formData) throws FormException {
 			
-			return new GithubAuthorizationStrategy();
+			
+			
+			String adminUserNames = formData.getString("adminUserNames");;
+			boolean authorizedReadPermission =formData.getBoolean("authenticatedUserReadPermission");
+			
+			return new GithubAuthorizationStrategy(adminUserNames, authorizedReadPermission);
 		}
 
         
