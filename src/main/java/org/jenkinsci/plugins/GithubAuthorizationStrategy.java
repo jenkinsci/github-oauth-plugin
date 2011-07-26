963bae605d193d5a2ef9aa6a6c7a3024c00ce100
Michael O'Cleirigh <michael.ocleirigh@rivulet.ca>
2011-07-25 20:07:17 -0400

    Fix AuthorizationStrategy serialization bug
    Thanks to lordkev for pointing out that the 0.1 release didn't really work.
    
    I tracked the problem to how the custom converter didn't serialize properly so that on jenkins restart
    it went back to the basic authorization strategy.
    
    Because authorization now works I have changed the permissioning back to require and authenticated user.
    
    No more anonymous read permission needed.

--- src/main/java/org/jenkinsci/plugins/GithubAuthorizationStrategy.java
+++ src/main/java/org/jenkinsci/plugins/GithubAuthorizationStrategy.java
@@ -4,38 +4,18 @@
 package org.jenkinsci.plugins;
 
 import hudson.Extension;
-import hudson.diagnosis.OldDataMonitor;
 import hudson.model.Descriptor;
 import hudson.security.ACL;
 import hudson.security.AuthorizationStrategy;
-import hudson.security.GlobalMatrixAuthorizationStrategy;
 import hudson.security.Permission;
-import hudson.util.RobustReflectionConverter;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
-import java.util.SortedMap;
-import java.util.TreeMap;
-import java.util.Map.Entry;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import net.sf.json.JSONObject;
 
 import org.acegisecurity.Authentication;
-import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.StaplerRequest;
-
-import com.thoughtworks.xstream.converters.Converter;
-import com.thoughtworks.xstream.converters.MarshallingContext;
-import com.thoughtworks.xstream.converters.UnmarshallingContext;
-import com.thoughtworks.xstream.io.HierarchicalStreamReader;
-import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 
 /**
  * @author mocleiri
@@ -76,6 +56,16 @@ public class GithubAuthorizationStrategy extends AuthorizationStrategy {
 				if (adminUserNameList.contains(candidateName)) 
 					// if they are an admin then they have permission
 					return true;
+				
+				if (authenticatedUserReadPermission) {
+
+					String[] parts = permission.getId().split("\\.");
+					if (parts[parts.length - 1].toLowerCase().equals("read"))
+
+						// if we support authenticated read and this is a read
+						// request we allow it
+						return true;
+				}
 					
 
 				for (String organizationName : this.organizationNameList) {
@@ -107,23 +97,18 @@ public class GithubAuthorizationStrategy extends AuthorizationStrategy {
 					// give system user full access
 					return true;
 				}
+				
+				if (a.getName().equals("anonymous")) {
+					// deny anonymous users
+					// everyone must be logged in 
+					return false;
+				}
 
 				if (adminUserNameList.contains(p)) {
 					// if they are an admin then they have all permissions
 					return true;
-				} else {
-					
-					// anonymous must have read access at the root level
-					if (a.getName().equals("anonymous") || authenticatedUserReadPermission) {
-
-						String[] parts = permission.getId().split("\\.");
-						if (parts[parts.length - 1].toLowerCase().equals("read"))
-
-							// if we support authenticated read and this is a read
-							// request we allow it
-							return true;
-					}
 				}
+
 				
 				return false;
 
@@ -176,9 +161,7 @@ public class GithubAuthorizationStrategy extends AuthorizationStrategy {
 			boolean authenticatedUserReadPermission, String organizationNames) {
 		super();
 		
-//		if (adminUserNames == null || adminUserNames.trim().length() == 0)
-//			throw new RuntimeException ("admin");
-		
+	
 		this.adminUserNames = adminUserNames;
 		this.authenticatedUserReadPermission = authenticatedUserReadPermission;
 		this.organizationNames = organizationNames;
@@ -260,75 +243,5 @@ public class GithubAuthorizationStrategy extends AuthorizationStrategy {
 		public String getHelpFile() {
 			return "/plugin/github-oauth/help/help-authorization-strategy.html";
 		}
-
-//		
-//		@Override
-//		public GithubAuthorizationStrategy newInstance(StaplerRequest req,
-//				JSONObject formData) throws FormException {
-//
-//			String adminUserNames = formData.getString(ADMIN_USER_NAMES);
-//			
-//			boolean authorizedReadPermission = formData
-//					.getBoolean(AUTHENTICATED_USER_READ_PERMISSION);
-//
-//			String organizationNames = formData.getString(ORGANIZATION_NAMES);
-//
-//			return new GithubAuthorizationStrategy(adminUserNames,
-//					authorizedReadPermission, organizationNames);
-//		}
-
 	}
-	
-	public static class ConverterImpl implements Converter {
-        public boolean canConvert(Class type) {
-            if (type==GithubAuthorizationStrategy.class)
-            	return true;
-            else
-            	return false;
-        }
-
-        public void marshal(Object source, HierarchicalStreamWriter writer, MarshallingContext context) {
-        	GithubAuthorizationStrategy strategy = (GithubAuthorizationStrategy)source;
-
-            // Output in alphabetical order for readability.
-        	
-        	writer.startNode(ADMIN_USER_NAMES);
-        	writer.setValue(strategy.getAdminUserNames());
-        	writer.endNode();
-        	
-        	writer.startNode(ORGANIZATION_NAMES);
-        	writer.setValue(strategy.getOrganizationNames());
-        	writer.endNode();
-        	
-        	writer.startNode(AUTHENTICATED_USER_READ_PERMISSION);
-        	writer.setValue(String.valueOf(strategy.isAuthenticatedUserReadPermission()));
-        	writer.endNode();
-        	
-
-        }
-
-        public Object unmarshal(HierarchicalStreamReader reader, final UnmarshallingContext context) {
-
-                reader.moveDown();
-
-                String adminUserNames = reader.getValue();
-                
-                reader.moveUp();
-                
-                reader.moveDown();
-                
-                String organizationNames = reader.getValue();
-                
-                reader.moveUp();
-                
-                reader.moveDown();
-                
-                boolean authorizedReadPermission = Boolean.valueOf(reader.getValue());
-         
-			return new GithubAuthorizationStrategy(adminUserNames,
-					authorizedReadPermission, organizationNames);
-        }
-
-      
-    }
 }
