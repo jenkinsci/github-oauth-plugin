9361c8dab1448ddbb9e5a54ea44e57698942592c
Michael O'Cleirigh <michael.ocleirigh@rivulet.ca>
2011-09-08 01:43:43 -0400

    Split out ACL from Authorization Strategy.
    This splits out the actual ACL from the Authorization Strategy.
    
    At first this was an attempt to isolate a serialization issue but it turned out that this changed the naming that the jelly wiring expected and prevented the existing data to be resolved properly.
    
    This commit uses delegate methods and some string conversion to let the details be shown properly.

--- src/main/java/org/jenkinsci/plugins/GithubAuthorizationStrategy.java
+++ src/main/java/org/jenkinsci/plugins/GithubAuthorizationStrategy.java
@@ -38,136 +38,7 @@ import org.kohsuke.stapler.Stapler;
  */
 public class GithubAuthorizationStrategy extends AuthorizationStrategy {
 
-	private static final String ORGANIZATION_NAMES = "organizationNames";
-	private static final String AUTHENTICATED_USER_READ_PERMISSION = "authenticatedUserReadPermission";
-	private static final String ADMIN_USER_NAMES = "adminUserNames";
-
-	private static class GithubRequireOrganizationMembershipACL extends ACL {
-
-		private final List<String> organizationNameList;
-		private final List<String> adminUserNameList;
-		private final boolean authenticatedUserReadPermission;
-		private final boolean allowGithubWebHookPermission;
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * hudson.security.ACL#hasPermission(org.acegisecurity.Authentication,
-		 * hudson.security.Permission)
-		 */
-		@Override
-		public boolean hasPermission(Authentication a, Permission permission) {
-
-			if (a != null && a instanceof GithubAuthenticationToken) {
-
-				GithubAuthenticationToken authenticationToken = (GithubAuthenticationToken) a;
-
-				String candidateName = a.getName();
-
-				if (adminUserNameList.contains(candidateName))
-					// if they are an admin then they have permission
-					return true;
-
-				if (authenticatedUserReadPermission) {
-
-					String[] parts = permission.getId().split("\\.");
-					if (parts[parts.length - 1].toLowerCase().equals("read"))
-
-						// if we support authenticated read and this is a read
-						// request we allow it
-						return true;
-				}
-
-				for (String organizationName : this.organizationNameList) {
-
-					if (authenticationToken.hasOrganizationPermission(
-							candidateName, organizationName)) {
-
-						String[] parts = permission.getId().split("\\.");
-
-						String test = parts[parts.length - 1].toLowerCase();
-
-						if (test.equals("read") || test.equals("build"))
-							// check the permission
-							return true;
-					}
-
-				}
-
-				// no match.
-				return false;
-
-			} else {
-
-				String p = a.getName();
-
-				if (p.equals(SYSTEM.getPrincipal())) {
-					// give system user full access
-					return true;
-				}
-
-				if (a.getName().equals("anonymous")) {
-					// deny anonymous users
-					// everyone must be logged in
-					String requestURI = Stapler.getCurrentRequest()
-							.getOriginalRequestURI();
-
-					if (requestURI.matches("^/github-webhook$") && allowGithubWebHookPermission == true) {
-
-							// allow if the permission was configured.
-
-							if (permission.getId().equals(
-									"hudson.model.Hudson.Read")
-									|| permission.getId().equals(
-											"hudson.model.Item.Read"))
-								return true;
-
-
-						// else fall through to false.
-					}
-					return false;
-				}
-
-				if (adminUserNameList.contains(p)) {
-					// if they are an admin then they have all permissions
-					return true;
-				}
-
-				return false;
-
-			}
-
-		}
-
-		public GithubRequireOrganizationMembershipACL(String adminUserNames,
-				String organizationNames,
-				boolean authenticatedUserReadPermission, boolean allowGithubWebHookPermission) {
-			super();
-			this.authenticatedUserReadPermission = authenticatedUserReadPermission;
-			this.allowGithubWebHookPermission = allowGithubWebHookPermission;
-
-			this.adminUserNameList = new LinkedList<String>();
-
-			String[] parts = adminUserNames.split(",");
-
-			for (String part : parts) {
-				adminUserNameList.add(part);
-			}
-
-			this.organizationNameList = new LinkedList<String>();
-
-			parts = organizationNames.split(",");
-
-			for (String part : parts) {
-				organizationNameList.add(part);
-			}
-
-		}
-
-	}
-
-	/**
+		/**
 	 * 
 	 */
 	@DataBoundConstructor
@@ -179,7 +50,7 @@ public class GithubAuthorizationStrategy extends AuthorizationStrategy {
 				organizationNames, authenticatedUserReadPermission, allowGithubWebHookPermission);
 	}
 
-	private GithubRequireOrganizationMembershipACL rootACL = null;
+	private final GithubRequireOrganizationMembershipACL rootACL;
 
 	/*
 	 * (non-Javadoc)
@@ -192,6 +63,8 @@ public class GithubAuthorizationStrategy extends AuthorizationStrategy {
 		return rootACL;
 
 	}
+	
+	
 
 	/*
 	 * (non-Javadoc)
@@ -202,6 +75,52 @@ public class GithubAuthorizationStrategy extends AuthorizationStrategy {
 	public Collection<String> getGroups() {
 		return new ArrayList<String>(0);
 	}
+
+	  private Object readResolve() {
+		  return this;
+	  }
+	  
+	  
+	  
+	/**
+	 * @return
+	 * @see org.jenkinsci.plugins.GithubRequireOrganizationMembershipACL#getOrganizationNameList()
+	 */
+	public String getOrganizationNames() {
+		return StringUtils.join(rootACL.getOrganizationNameList(), ",");
+	}
+
+
+
+	/**
+	 * @return
+	 * @see org.jenkinsci.plugins.GithubRequireOrganizationMembershipACL#getAdminUserNameList()
+	 */
+	public String getAdminUserNames() {
+		return StringUtils.join(rootACL.getAdminUserNameList(), ",");
+	}
+
+
+
+	/**
+	 * @return
+	 * @see org.jenkinsci.plugins.GithubRequireOrganizationMembershipACL#isAuthenticatedUserReadPermission()
+	 */
+	public boolean isAuthenticatedUserReadPermission() {
+		return rootACL.isAuthenticatedUserReadPermission();
+	}
+
+
+
+	/**
+	 * @return
+	 * @see org.jenkinsci.plugins.GithubRequireOrganizationMembershipACL#isAllowGithubWebHookPermission()
+	 */
+	public boolean isAllowGithubWebHookPermission() {
+		return rootACL.isAllowGithubWebHookPermission();
+	}
+
+
 
 	@Extension
 	public static final class DescriptorImpl extends
---
+++ src/main/java/org/jenkinsci/plugins/GithubRequireOrganizationMembershipACL.java
@@ -0,0 +1,162 @@
+/**
+ * 
+ */
+package org.jenkinsci.plugins;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.acegisecurity.Authentication;
+import org.kohsuke.stapler.Stapler;
+
+import hudson.security.ACL;
+import hudson.security.Permission;
+
+/**
+ * @author Mike
+ *
+ */
+public class GithubRequireOrganizationMembershipACL extends ACL {
+
+
+		private final List<String> organizationNameList;
+		private final List<String> adminUserNameList;
+		private final boolean authenticatedUserReadPermission;
+		private final boolean allowGithubWebHookPermission;
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * hudson.security.ACL#hasPermission(org.acegisecurity.Authentication,
+		 * hudson.security.Permission)
+		 */
+		@Override
+		public boolean hasPermission(Authentication a, Permission permission) {
+
+			if (a != null && a instanceof GithubAuthenticationToken) {
+
+				GithubAuthenticationToken authenticationToken = (GithubAuthenticationToken) a;
+
+				String candidateName = a.getName();
+
+				if (adminUserNameList.contains(candidateName))
+					// if they are an admin then they have permission
+					return true;
+
+				if (authenticatedUserReadPermission) {
+
+					String[] parts = permission.getId().split("\\.");
+					if (parts[parts.length - 1].toLowerCase().equals("read"))
+
+						// if we support authenticated read and this is a read
+						// request we allow it
+						return true;
+				}
+
+				for (String organizationName : this.organizationNameList) {
+
+					if (authenticationToken.hasOrganizationPermission(
+							candidateName, organizationName)) {
+
+						String[] parts = permission.getId().split("\\.");
+
+						String test = parts[parts.length - 1].toLowerCase();
+
+						if (test.equals("read") || test.equals("build"))
+							// check the permission
+							return true;
+					}
+
+				}
+
+				// no match.
+				return false;
+
+			} else {
+
+				String p = a.getName();
+
+				if (p.equals(SYSTEM.getPrincipal())) {
+					// give system user full access
+					return true;
+				}
+
+				if (a.getName().equals("anonymous")) {
+					// deny anonymous users
+					// everyone must be logged in
+					String requestURI = Stapler.getCurrentRequest()
+							.getOriginalRequestURI();
+
+					if (requestURI.matches("^/github-webhook$") && allowGithubWebHookPermission == true) {
+
+							// allow if the permission was configured.
+
+							if (permission.getId().equals(
+									"hudson.model.Hudson.Read")
+									|| permission.getId().equals(
+											"hudson.model.Item.Read"))
+								return true;
+
+
+						// else fall through to false.
+					}
+					return false;
+				}
+
+				if (adminUserNameList.contains(p)) {
+					// if they are an admin then they have all permissions
+					return true;
+				}
+
+				return false;
+
+			}
+
+		}
+
+		public GithubRequireOrganizationMembershipACL(String adminUserNames,
+				String organizationNames,
+				boolean authenticatedUserReadPermission, boolean allowGithubWebHookPermission) {
+			super();
+			this.authenticatedUserReadPermission = authenticatedUserReadPermission;
+			this.allowGithubWebHookPermission = allowGithubWebHookPermission;
+
+			this.adminUserNameList = new LinkedList<String>();
+
+			String[] parts = adminUserNames.split(",");
+
+			for (String part : parts) {
+				adminUserNameList.add(part);
+			}
+
+			this.organizationNameList = new LinkedList<String>();
+
+			parts = organizationNames.split(",");
+
+			for (String part : parts) {
+				organizationNameList.add(part);
+			}
+
+		}
+
+		public List<String> getOrganizationNameList() {
+			return organizationNameList;
+		}
+
+		public List<String> getAdminUserNameList() {
+			return adminUserNameList;
+		}
+
+		public boolean isAuthenticatedUserReadPermission() {
+			return authenticatedUserReadPermission;
+		}
+
+		public boolean isAllowGithubWebHookPermission() {
+			return allowGithubWebHookPermission;
+		}
+
+		
+		
+
+}
