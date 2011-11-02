3e35272feea337357546eeb71a41968ead5b3fa5
Michael O'Cleirigh <michael.ocleirigh@rivulet.ca>
2011-11-01 23:38:01 -0400

    Add Converter implementation to GithubSecurityRealm
    This is the real fix for the 0.7 upgrade issue.  Now we can control programatically how the SecurityRealm is loaded by XStream and insert the DEFAULT_URI when it is missing.

--- src/main/java/org/jenkinsci/plugins/GithubSecurityRealm.java
+++ src/main/java/org/jenkinsci/plugins/GithubSecurityRealm.java
@@ -29,10 +29,19 @@ package org.jenkinsci.plugins;
 import hudson.Extension;
 import hudson.Util;
 import hudson.model.Descriptor;
+import hudson.model.Fingerprint.RangeSet;
 import hudson.security.GroupDetails;
+import hudson.security.Permission;
 import hudson.security.SecurityRealm;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.Map.Entry;
 import java.util.logging.Logger;
 
 import org.acegisecurity.Authentication;
@@ -58,6 +67,13 @@ import org.kohsuke.stapler.HttpResponses;
 import org.kohsuke.stapler.StaplerRequest;
 import org.springframework.dao.DataAccessException;
 
+import com.thoughtworks.xstream.converters.ConversionException;
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+
 /**
  * 
  * Implementation of the AbstractPasswordBasedSecurityRealm that uses github
@@ -69,33 +85,146 @@ import org.springframework.dao.DataAccessException;
 public class GithubSecurityRealm extends SecurityRealm {
 
 	private static final String DEFAULT_URI = "https://github.com";
-	
+
 	private String githubUri;
 	private String clientID;
 	private String clientSecret;
 
 	@DataBoundConstructor
-	public GithubSecurityRealm(String githubUri, String clientID, String clientSecret) {
+	public GithubSecurityRealm(String githubUri, String clientID,
+			String clientSecret) {
 		super();
 
 		this.githubUri = Util.fixEmptyAndTrim(githubUri);
 		this.clientID = Util.fixEmptyAndTrim(clientID);
 		this.clientSecret = Util.fixEmptyAndTrim(clientSecret);
-		
+
+	}
+
+	private GithubSecurityRealm() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param githubUri
+	 *            the githubUri to set
+	 */
+	private void setGithubUri(String githubUri) {
+		this.githubUri = githubUri;
+	}
+
+	/**
+	 * @param clientID
+	 *            the clientID to set
+	 */
+	private void setClientID(String clientID) {
+		this.clientID = clientID;
+	}
+
+	/**
+	 * @param clientSecret
+	 *            the clientSecret to set
+	 */
+	private void setClientSecret(String clientSecret) {
+		this.clientSecret = clientSecret;
+	}
+
+	public static final class ConverterImpl implements Converter {
+
+		public boolean canConvert(Class type) {
+			return type == GithubSecurityRealm.class;
+		}
+
+		public void marshal(Object source, HierarchicalStreamWriter writer,
+				MarshallingContext context) {
+
+			GithubSecurityRealm realm = (GithubSecurityRealm) source;
+
+			writer.startNode("githubUri");
+			writer.setValue(realm.getGithubUri());
+			writer.endNode();
+
+			writer.startNode("clientID");
+			writer.setValue(realm.getClientID());
+			writer.endNode();
+
+			writer.startNode("secretID");
+			writer.setValue(realm.getClientSecret());
+			writer.endNode();
+
+		}
+
+		public Object unmarshal(HierarchicalStreamReader reader,
+				UnmarshallingContext context) {
+
+			String node = reader.getNodeName();
+
+			reader.moveDown();
+
+			GithubSecurityRealm realm = new GithubSecurityRealm();
+
+			node = reader.getNodeName();
+
+			String value = reader.getValue();
+
+			setValue(realm, node, value);
+
+			reader.moveUp();
+
+			reader.moveDown();
+
+			node = reader.getNodeName();
+
+			value = reader.getValue();
+
+			setValue(realm, node, value);
+
+			reader.moveUp();
+
+			if (reader.hasMoreChildren()) {
+				reader.moveDown();
+
+				node = reader.getNodeName();
+
+				value = reader.getValue();
+
+				setValue(realm, node, value);
+
+				reader.moveUp();
+			}
+
+			if (realm.getGithubUri() == null) {
+				realm.setGithubUri(DEFAULT_URI);
+			}
+
+			return realm;
+		}
+
+		private void setValue(GithubSecurityRealm realm, String node,
+				String value) {
+
+			if (node.toLowerCase().equals("clientid")) {
+				realm.setClientID(value);
+			} else if (node.toLowerCase().equals("clientsecret")) {
+				realm.setClientSecret(value);
+			} else if (node.toLowerCase().equals("githuburi")) {
+				realm.setGithubUri(value);
+			} else
+				throw new ConversionException("invalid node value = " + node);
+
+		}
+
 	}
 
 	/**
 	 * @return the uri to Github (varies for Github Enterprise Edition)
 	 */
 	public String getGithubUri() {
-		
-		if (githubUri == null)
-			return DEFAULT_URI;
-		else
-			return githubUri;
+
+		return githubUri;
 	}
-	
-     /**
+
+	/**
 	 * @return the clientID
 	 */
 	public String getClientID() {
@@ -109,25 +238,23 @@ public class GithubSecurityRealm extends SecurityRealm {
 		return clientSecret;
 	}
 
-	
-	
-//	@Override
-//	public Filter createFilter(FilterConfig filterConfig) {
-//		
-//		return new GithubOAuthAuthenticationFilter();
-//	}
+	// @Override
+	// public Filter createFilter(FilterConfig filterConfig) {
+	//
+	// return new GithubOAuthAuthenticationFilter();
+	// }
 
 	public HttpResponse doCommenceLogin(@Header("Referer") final String referer)
 			throws IOException {
 
-		return new HttpRedirect(
-				githubUri + "/login/oauth/authorize?client_id="
-						+ clientID);
-		
+		return new HttpRedirect(githubUri + "/login/oauth/authorize?client_id="
+				+ clientID);
+
 		// we only need the readonly scope to get the group membership info.
-		// if we extend for other repo aware details the token scope may need to be specified.
+		// if we extend for other repo aware details the token scope may need to
+		// be specified.
 		// this can be done by adding a scope paramter to the above url.
-		//  + "&scope=user,public_repo,repo"
+		// + "&scope=user,public_repo,repo"
 
 	}
 
@@ -142,10 +269,9 @@ public class GithubSecurityRealm extends SecurityRealm {
 
 		Log.info("test");
 
-		HttpPost httpost = new HttpPost(
-				githubUri + "/login/oauth/access_token?" + "client_id="
-						+ clientID + "&" + "client_secret=" + clientSecret
-						+ "&" + "code=" + code);
+		HttpPost httpost = new HttpPost(githubUri
+				+ "/login/oauth/access_token?" + "client_id=" + clientID + "&"
+				+ "client_secret=" + clientSecret + "&" + "code=" + code);
 
 		DefaultHttpClient httpclient = new DefaultHttpClient();
 
@@ -199,15 +325,15 @@ public class GithubSecurityRealm extends SecurityRealm {
 
 	@Override
 	public SecurityComponents createSecurityComponents() {
-		
-           
-		
+
 		return new SecurityComponents(new AuthenticationManager() {
-			
-				 public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-		                if (authentication instanceof GithubAuthenticationToken)
-		                    return authentication;
-		                throw new BadCredentialsException("Unexpected authentication type: "+authentication);
+
+			public Authentication authenticate(Authentication authentication)
+					throws AuthenticationException {
+				if (authentication instanceof GithubAuthenticationToken)
+					return authentication;
+				throw new BadCredentialsException(
+						"Unexpected authentication type: " + authentication);
 			}
 		}, new UserDetailsService() {
 			public UserDetails loadUserByUsername(String username)
@@ -217,7 +343,6 @@ public class GithubSecurityRealm extends SecurityRealm {
 		});
 	}
 
-	
 	@Override
 	public String getLoginUrl() {
 
