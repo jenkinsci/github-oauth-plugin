4eeb9b55a9e9a01c589013f8e4a5b493b1127f34
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-05-31 08:46:50 -0700

    [FIXED JENKINS-23254]
    Consider WORKSPACE to be a part of the read permission

--- src/main/java/org/jenkinsci/plugins/GithubRequireOrganizationMembershipACL.java
+++ src/main/java/org/jenkinsci/plugins/GithubRequireOrganizationMembershipACL.java
@@ -210,6 +210,7 @@ public class GithubRequireOrganizationMembershipACL extends ACL {
 
 	private boolean checkReadPermission(Permission permission) {
 		if (permission.getId().equals("hudson.model.Hudson.Read")
+                || permission.getId().equals("hudson.model.Item.Workspace")
 				|| permission.getId().equals("hudson.model.Item.Read")) {
 			return true;
 		} else
